cbf956369411b1588522c8b5eaaa7601139a5883
BenjaminSaldman <93070344+BenjaminSaldman@users.noreply.github.com>
2025-05-18 21:05:05 +0300

    Create Demo.cpp

---
+++ Demo.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include "MyContainer.hpp"
+using namespace ariel;
+int main() {
+    MyContainer<int> container;
+    container.addElement(7);
+    container.addElement(15);
+    container.addElement(6);
+    container.addElement(1);
+    container.addElement(2);
+
+    std::cout << "Size of container: " << container.size() << std::endl;
+
+    MyContainer::AscendingOrder ascIter(container);
+    for (auto it = ascIter.begin_ascending_order(); it != ascIter.end_ascending_order(); ++it) {
+        std::cout << *it << ' ';   // 1 2 6 7 15
+    }
+    std::cout << std::endl;
+
+    MyContainer::DescendingOrder descIter(container);
+    for (auto it = descIter.begin_descending_order(); it != descIter.end_descending_order(); ++it) {
+        std::cout << *it << ' ';  // 15 7 6 2 1
+    }
+    std::cout << std::endl;
+
+    MyContainer::SideCrossOrder sidecrossIter(container);
+    for (auto it = sidecrossIter.begin_side_cross_order(); it != sidecrossIter.end_side_cross_order(); ++it) {
+        std::cout << *it << ' ';  // 1 15 2 7 6
+    }
+    std::cout << std::endl;
+
+    MyContainer::ReverseOrder revIter(container);
+    for (auto it = revIter.begin_reverse_order(); it != revIter.end_reverse_order(); ++it) {
+        std::cout << *it << ' ';  // 2 1 6 15 7
+    }
+    std::cout << std::endl;
+
+    MyContainer::Order orderIter(container);
+    for (auto it = orderIter.begin_order(); it != orderIter.end_order(); ++it) {
+        std::cout << *it << ' ';  // 7 15 6 1 2
+    }
+    std::cout << std::endl;
+
+    MyContainer::MiddleOutOrder midoutIter(container);
+    for (auto it = midoutIter.begin_middle_out_order(); it != midoutIter.end_middle_out_order(); ++it) {
+        std::cout << *it << ' ';  // 6 15 1 7 2
+    }
+    std::cout << std::endl;
+
+  
+
+    return 0;
+}
