dc51dfca8b5ecec6921cef207485bf579c92fe61
BenjaminSaldman <93070344+BenjaminSaldman@users.noreply.github.com>
2025-05-18 21:15:34 +0300

    Update Demo.cpp

--- Demo.cpp
+++ Demo.cpp
@@ -11,37 +11,37 @@ int main() {
 
     std::cout << "Size of container: " << container.size() << std::endl;
 
-    MyContainer::AscendingOrder ascIter(container);
+    MyContainer<int>::AscendingOrder ascIter(container);
     for (auto it = ascIter.begin_ascending_order(); it != ascIter.end_ascending_order(); ++it) {
         std::cout << *it << ' ';   // 1 2 6 7 15
     }
     std::cout << std::endl;
 
-    MyContainer::DescendingOrder descIter(container);
+    MyContainer<int>::DescendingOrder descIter(container);
     for (auto it = descIter.begin_descending_order(); it != descIter.end_descending_order(); ++it) {
         std::cout << *it << ' ';  // 15 7 6 2 1
     }
     std::cout << std::endl;
 
-    MyContainer::SideCrossOrder sidecrossIter(container);
+    MyContainer<int>::SideCrossOrder sidecrossIter(container);
     for (auto it = sidecrossIter.begin_side_cross_order(); it != sidecrossIter.end_side_cross_order(); ++it) {
         std::cout << *it << ' ';  // 1 15 2 7 6
     }
     std::cout << std::endl;
 
-    MyContainer::ReverseOrder revIter(container);
+    MyContainer<int>::ReverseOrder revIter(container);
     for (auto it = revIter.begin_reverse_order(); it != revIter.end_reverse_order(); ++it) {
         std::cout << *it << ' ';  // 2 1 6 15 7
     }
     std::cout << std::endl;
 
-    MyContainer::Order orderIter(container);
+    MyContainer<int>::Order orderIter(container);
     for (auto it = orderIter.begin_order(); it != orderIter.end_order(); ++it) {
         std::cout << *it << ' ';  // 7 15 6 1 2
     }
     std::cout << std::endl;
 
-    MyContainer::MiddleOutOrder midoutIter(container);
+    MyContainer<int>::MiddleOutOrder midoutIter(container);
     for (auto it = midoutIter.begin_middle_out_order(); it != midoutIter.end_middle_out_order(); ++it) {
         std::cout << *it << ' ';  // 6 15 1 7 2
     }
