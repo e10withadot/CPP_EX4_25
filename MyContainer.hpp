4d7e4f7328c8497bc4ce312384ce1d2508c03eb3
e10withadot <e10withadot@gmail.com>
2025-06-12 10:52:35 +0300

    made basic MyContainer impl

--- MyContainer.hpp
+++ MyContainer.hpp
@@ -3,13 +3,30 @@ namespace contain {
 	class MyContainer {
 		private:
 			int SIZE;
-			K* ARR;
+			int ITEMS;
+			K ARR[];
 		public:
-			MyContainer() {}
-			MyContainer(int size){}
-			MyContainer(K arr[]) {}
-			void add() {}
-			void remove() {}
-			int size() {}
+			MyContainer() : ITEMS(0), SIZE(0) {}
+			MyContainer(K arr[]) {
+				int size = sizeof(arr) / sizeof(arr[0]);
+				SIZE = size;
+				ITEMS = size;
+				for (int i = 0; i < size; i++) {
+					ARR[i] = arr[i];
+				}
+
+}
+			void add(K item) {
+				if (ITEMS < SIZE-1) {
+					ARR[ITEMS] = item;
+					ITEMS++;
+				}
+}
+			void remove() {
+				ARR[ITEMS--] = nullptr;
+}
+			int size() {
+				return ITEMS;
+}
 	}
 }
