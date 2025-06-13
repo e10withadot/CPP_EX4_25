d3e32a9bc915bf6521a4335e3fedf2aa0927c7df
e10withadot <e10withadot@gmail.com>
2025-06-13 16:36:25 +0300

    added sidecrossorder

--- Iterators.hpp
+++ Iterators.hpp
@@ -35,9 +35,39 @@ public:
   DescendingOrder(const DescendingOrder &other)
       : SORT(other.SORT), IT(SORT.end() - (other.SORT.end() - other.IT)) {}
 
-  bool hasNext() const { return IT != SORT.head(); }
+  bool hasNext() const { return IT != SORT.begin(); }
   const K &next() { return *(IT--); }
   void reset() { IT = SORT.end(); }
+};
+
+template <class K> class SideCrossOrder {
+private:
+  vector<K> SORT;
+  typename vector<K>::const_iterator IT1;
+  typename vector<K>::const_iterator IT2;
+  bool is_one = true;
+
+public:
+  SideCrossOrder(const MyContainer<K> &container) : SORT(container.ARR) {
+    std::sort(SORT.begin(), SORT.end());
+    IT1 = SORT.begin();
+    IT2 = SORT.end();
+  }
+  SideCrossOrder(const SideCrossOrder &other)
+      : SORT(other.SORT), IT1(SORT.begin() + (other.IT1 - other.SORT.begin())),
+        IT2(SORT.end() - (other.SORT.end() - other.IT2)) {}
+
+  bool hasNext() const { return IT1 != SORT.end() || IT2 != SORT.begin(); }
+  const K &next() {
+    if (is_one)
+      return *(IT1++);
+    else
+      return *(IT2--);
+  }
+  void reset() {
+    IT1 = SORT.begin();
+    IT2 = SORT.end();
+  }
 };
 
 template <class K> class ReverseOrder {
@@ -52,7 +82,7 @@ public:
   ReverseOrder(const ReverseOrder &other)
       : VEC(other.VEC), IT(VEC.end() - (other.VEC.end() - other.IT)) {}
 
-  bool hasNext() const { return IT != VEC.head(); }
+  bool hasNext() const { return IT != VEC.begin(); }
   const K &next() { return *(IT--); }
   void reset() { IT = VEC.end(); }
 };
