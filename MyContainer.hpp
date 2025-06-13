2a8ae4b17ba905145558589c24e8489097f5d44c
e10withadot <e10withadot@gmail.com>
2025-06-13 18:57:02 +0300

    merged Iterators and MyContainer

--- MyContainer.hpp
+++ MyContainer.hpp
@@ -1,10 +1,167 @@
+#ifndef MYCONTAINER_HPP
+#define MYCONTAINER_HPP
+
 #include <algorithm>
 #include <iostream>
 #include <vector>
 using namespace std;
 
 namespace contain {
-template <class K> class MyContainer {
+template <typename K> class MyContainer;
+template <typename K> class AscendingOrder {
+private:
+  vector<K> SORT;
+  typename vector<K>::const_iterator IT;
+
+public:
+  AscendingOrder(const MyContainer<K> &container) : SORT(container.ARR) {
+    std::sort(SORT.begin(), SORT.end());
+    IT = SORT.begin();
+  }
+  AscendingOrder(const AscendingOrder &other)
+      : SORT(other.SORT), IT(SORT.begin() + (other.IT - other.SORT.begin())) {}
+
+  bool hasNext() const { return IT != SORT.end(); }
+  const K &next() { return *(IT++); }
+  void reset() { IT = SORT.begin(); }
+};
+
+template <typename K> class DescendingOrder {
+private:
+  vector<K> SORT;
+  typename vector<K>::const_iterator IT;
+
+public:
+  DescendingOrder(const MyContainer<K> &container) : SORT(container.ARR) {
+    std::sort(SORT.begin(), SORT.end());
+    IT = SORT.end();
+  }
+  DescendingOrder(const DescendingOrder &other)
+      : SORT(other.SORT), IT(SORT.end() - (other.SORT.end() - other.IT)) {}
+
+  bool hasNext() const { return IT != SORT.begin(); }
+  const K &next() { return *(IT--); }
+  void reset() { IT = SORT.end(); }
+};
+
+template <typename K> class SideCrossOrder {
+private:
+  vector<K> SORT;
+  typename vector<K>::const_iterator IT1;
+  typename vector<K>::const_iterator IT2;
+  bool is_one = true;
+
+public:
+  SideCrossOrder(const MyContainer<K> &container) : SORT(container.ARR) {
+    std::sort(SORT.begin(), SORT.end());
+    IT1 = SORT.begin();
+    IT2 = SORT.end();
+  }
+  SideCrossOrder(const SideCrossOrder &other)
+      : SORT(other.SORT), IT1(SORT.begin() + (other.IT1 - other.SORT.begin())),
+        IT2(SORT.end() - (other.SORT.end() - other.IT2)) {}
+
+  bool hasNext() const { return IT1 <= IT2; }
+  const K &next() {
+    if (is_one)
+      return *(IT1++);
+    else
+      return *(IT2--);
+  }
+  void reset() {
+    IT1 = SORT.begin();
+    IT2 = SORT.end();
+  }
+};
+
+template <typename K> class ReverseOrder {
+private:
+  vector<K> VEC;
+  typename vector<K>::const_iterator IT;
+
+public:
+  ReverseOrder(const MyContainer<K> &container) : VEC(container.ARR) {
+    IT = VEC.end();
+  }
+  ReverseOrder(const ReverseOrder &other)
+      : VEC(other.VEC), IT(VEC.end() - (other.VEC.end() - other.IT)) {}
+
+  bool hasNext() const { return IT != VEC.begin(); }
+  const K &next() { return *(IT--); }
+  void reset() { IT = VEC.end(); }
+};
+
+template <typename K> class Order {
+private:
+  vector<K> VEC;
+  typename vector<K>::const_iterator IT;
+
+public:
+  Order(const MyContainer<K> &container) : VEC(container.ARR) {
+    IT = VEC.begin();
+  }
+  Order(const Order &other)
+      : VEC(other.VEC), IT(VEC.begin() + (other.IT - other.VEC.begin())) {}
+
+  bool hasNext() const { return IT != VEC.end(); }
+  const K &next() { return *(IT++); }
+  void reset() { IT = VEC.begin(); }
+};
+
+template <typename K> class MiddleOutOrder {
+private:
+  vector<K> SORT;
+  typename vector<K>::const_iterator IT;
+
+  void buildOrder() {
+    vector<K> newsort;
+    newsort.reserve(SORT.size());
+    int mid = SORT.size() / 2;
+    newsort.push_back(SORT[mid]);
+    long unsigned int left = mid, right = mid;
+    bool go_left = true;
+    while (newsort.size() < SORT.size()) {
+      if (go_left) {
+        if (left > 0) {
+          --left;
+          newsort.push_back(SORT[left]);
+        }
+      } else {
+        if (right + 1 < SORT.size()) {
+          ++right;
+          newsort.push_back(SORT[right]);
+        }
+      }
+      go_left = !go_left;
+    }
+    SORT = newsort;
+  }
+
+public:
+  MiddleOutOrder(const MyContainer<K> &container) : SORT(container.ARR) {
+    sort(SORT.begin(), SORT.end());
+    buildOrder();
+    IT = SORT.begin();
+  }
+  MiddleOutOrder(const MiddleOutOrder &other)
+      : SORT(other.SORT), IT(SORT.begin() + (other.IT - other.SORT.begin())) {}
+
+  bool hasNext() const { return IT != SORT.end(); }
+  const K &next() { return *(IT++); }
+  void reset() {
+    sort(SORT.begin(), SORT.end());
+    buildOrder();
+    IT = SORT.begin();
+  }
+};
+template <typename K> class MyContainer {
+  friend class AscendingOrder<K>;
+  friend class DescendingOrder<K>;
+  friend class SideCrossOrder<K>;
+  friend class ReverseOrder<K>;
+  friend class Order<K>;
+  friend class MiddleOutOrder<K>;
+
 private:
   vector<K> ARR;
 
@@ -41,3 +198,4 @@ public:
   }
 };
 } // namespace contain
+#endif // MYCONTAINER_HPP
--- test.cpp
+++ test.cpp
@@ -1,108 +1,106 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
-#include "doctest.h"
 #include "MyContainer.hpp"
-#include "Iterators.hpp"
-
+#include "doctest.h"
 using namespace contain;
 
 TEST_CASE("MyContainer basic operations") {
-    SUBCASE("Empty container") {
-        MyContainer<int> container;
-        CHECK(container.size() == 0);
-    }
+  SUBCASE("Empty container") {
+    MyContainer<int> container;
+    CHECK(container.size() == 0);
+  }
 
-    SUBCASE("Container with initial array") {
-        int arr[] = {1, 2, 3, 4, 5};
-        MyContainer<int> container(arr, 5);
-        CHECK(container.size() == 5);
-    }
+  SUBCASE("Container with initial array") {
+    int arr[] = {1, 2, 3, 4, 5};
+    MyContainer<int> container(arr, 5);
+    CHECK(container.size() == 5);
+  }
 
-    SUBCASE("Add and remove operations") {
-        MyContainer<int> container;
-        container.add(1);
-        container.add(2);
-        container.add(2);
-        container.add(3);
-        CHECK(container.size() == 4);
-        
-        container.remove(2);
-        CHECK(container.size() == 2);
-    }
+  SUBCASE("Add and remove operations") {
+    MyContainer<int> container;
+    container.add(1);
+    container.add(2);
+    container.add(2);
+    container.add(3);
+    CHECK(container.size() == 4);
+
+    container.remove(2);
+    CHECK(container.size() == 2);
+  }
 }
 
 TEST_CASE("Iterator tests") {
-    int arr[] = {3, 1, 4, 1, 5, 9, 2, 6};
-    MyContainer<int> container(arr, 8);
+  int arr[] = {3, 1, 4, 1, 5, 9, 2, 6};
+  MyContainer<int> container(arr, 8);
 
-    SUBCASE("AscendingOrder iterator") {
-        AscendingOrder<int> iter(container);
-        CHECK(iter.hasNext() == true);
-        CHECK(iter.next() == 1);
-        CHECK(iter.next() == 1);
-        CHECK(iter.next() == 2);
-    }
+  SUBCASE("AscendingOrder iterator") {
+    AscendingOrder<int> iter(container);
+    CHECK(iter.hasNext() == true);
+    CHECK(iter.next() == 1);
+    CHECK(iter.next() == 1);
+    CHECK(iter.next() == 2);
+  }
 
-    SUBCASE("DescendingOrder iterator") {
-        DescendingOrder<int> iter(container);
-        CHECK(iter.hasNext() == true);
-        CHECK(iter.next() == 9);
-        CHECK(iter.next() == 6);
-        CHECK(iter.next() == 5);
-    }
+  SUBCASE("DescendingOrder iterator") {
+    DescendingOrder<int> iter(container);
+    CHECK(iter.hasNext() == true);
+    CHECK(iter.next() == 9);
+    CHECK(iter.next() == 6);
+    CHECK(iter.next() == 5);
+  }
 
-    SUBCASE("Order iterator (original order)") {
-        Order<int> iter(container);
-        CHECK(iter.hasNext() == true);
-        CHECK(iter.next() == 3);
-        CHECK(iter.next() == 1);
-        CHECK(iter.next() == 4);
-    }
+  SUBCASE("Order iterator (original order)") {
+    Order<int> iter(container);
+    CHECK(iter.hasNext() == true);
+    CHECK(iter.next() == 3);
+    CHECK(iter.next() == 1);
+    CHECK(iter.next() == 4);
+  }
 
-    SUBCASE("ReverseOrder iterator") {
-        ReverseOrder<int> iter(container);
-        CHECK(iter.hasNext() == true);
-        CHECK(iter.next() == 6);
-        CHECK(iter.next() == 2);
-        CHECK(iter.next() == 9);
-    }
+  SUBCASE("ReverseOrder iterator") {
+    ReverseOrder<int> iter(container);
+    CHECK(iter.hasNext() == true);
+    CHECK(iter.next() == 6);
+    CHECK(iter.next() == 2);
+    CHECK(iter.next() == 9);
+  }
 }
 
 TEST_CASE("Iterator reset functionality") {
-    int arr[] = {1, 2, 3, 4, 5};
-    MyContainer<int> container(arr, 5);
+  int arr[] = {1, 2, 3, 4, 5};
+  MyContainer<int> container(arr, 5);
 
-    SUBCASE("AscendingOrder reset") {
-        AscendingOrder<int> iter(container);
-        iter.next();
-        iter.next();
-        iter.reset();
-        CHECK(iter.next() == 1);
-    }
+  SUBCASE("AscendingOrder reset") {
+    AscendingOrder<int> iter(container);
+    iter.next();
+    iter.next();
+    iter.reset();
+    CHECK(iter.next() == 1);
+  }
 
-    SUBCASE("DescendingOrder reset") {
-        DescendingOrder<int> iter(container);
-        iter.next();
-        iter.next();
-        iter.reset();
-        CHECK(iter.next() == 5);
-    }
+  SUBCASE("DescendingOrder reset") {
+    DescendingOrder<int> iter(container);
+    iter.next();
+    iter.next();
+    iter.reset();
+    CHECK(iter.next() == 5);
+  }
 }
 
 TEST_CASE("Copy constructor tests") {
-    int arr[] = {1, 2, 3, 4, 5};
-    MyContainer<int> container(arr, 5);
+  int arr[] = {1, 2, 3, 4, 5};
+  MyContainer<int> container(arr, 5);
 
-    SUBCASE("AscendingOrder copy") {
-        AscendingOrder<int> iter1(container);
-        iter1.next(); // advance original iterator
-        AscendingOrder<int> iter2(iter1);
-        CHECK(iter2.next() == 2);
-    }
+  SUBCASE("AscendingOrder copy") {
+    AscendingOrder<int> iter1(container);
+    iter1.next(); // advance original iterator
+    AscendingOrder<int> iter2(iter1);
+    CHECK(iter2.next() == 2);
+  }
 
-    SUBCASE("DescendingOrder copy") {
-        DescendingOrder<int> iter1(container);
-        iter1.next(); // advance original iterator
-        DescendingOrder<int> iter2(iter1);
-        CHECK(iter2.next() == 4);
-    }
+  SUBCASE("DescendingOrder copy") {
+    DescendingOrder<int> iter1(container);
+    iter1.next(); // advance original iterator
+    DescendingOrder<int> iter2(iter1);
+    CHECK(iter2.next() == 4);
+  }
 }
