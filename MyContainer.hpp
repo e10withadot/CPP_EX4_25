d16f111f3f654faecd96a55f57d8734d6ffa79f9
e10withadot <e10withadot@gmail.com>
2025-06-13 11:12:00 +0300

    added docs for MyContainer

--- MyContainer.hpp
+++ MyContainer.hpp
@@ -12,19 +12,31 @@ public:
   MyContainer() : ARR() {}
   MyContainer(K arr[], int size) { ARR = vector<K>(arr, arr + size); }
 
+  /*
+   * Adds a new item to the end of the container.
+   */
   void add(K item) { ARR.push_back(item); }
+
+  /*
+   * Removes every instance of the item in the container.
+   */
   void remove(K item) {
     ARR.erase(std::remove(ARR.begin(), ARR.end(), item), ARR.end());
   }
+
+  /*
+   * Returns the container size.
+   */
   int size() { return ARR.size(); }
+
   friend ostream &operator<<(ostream &os, const MyContainer<K> &c) {
-    os << "[";
+    os << "[ ";
     for (int i = 0; i < c->ARR.size(); i++) {
       os << c->ARR[i];
       if (i != c->ARR.size() - 1)
         os << ", ";
     }
-    os << "]\n";
+    os << " ]\n";
     return os;
   }
 };
